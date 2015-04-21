a59e9d1719cbe50c30845985c8c61b66c912533f
vothanhkiet <vothanhkiet@gmail.com>
2015-04-21 18:31:51 +0700

    Clean up

--- server.js
+++ server.js
@@ -2,16 +2,12 @@
  * Created by kiettv on 4/20/15.
  */
 'use strict';
-var Promise = require('bluebird');
-var AWS = require('aws-sdk');
-
-var Uploader = require('./lib/upload');
-
+var Uploader = require('./lib');
 var opts = {
     aws: {
-        region: 'ap-southeast-1',
+        region: 'ap-southeast-1', //us-east-1 | us-west-2 | us-west-1 | eu-west-1 | eu-central-1 | ap-southeast-1 | ap-southeast-2 | ap-northeast-1 | sa-east-1
         path: 'test/',
-        acl: 'public-read',
+        acl: 'public-read', // 'private | public-read | public-read-write | authenticated-read | bucket-owner-read | bucket-owner-full-control'
         sslEnabled: true,
         maxRetries: 3,
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -23,38 +19,25 @@ var opts = {
             }
         }
     },
+    returnExif: false, // keep exif
+    resizeQuality: 80, // default resize quality, if quality for specific version is not set
+    tmpPrefix: 'gm-', // temporary file's prefix
     versions: [
         {
-            original: true
-        },
-        {
-            suffix: '-aaa',
-            quality: 80,
-            maxHeight: 300,
-            maxWidth: 400
+            original: true,
+            keep: true // not delete after upload
         },
         {
             suffix: '-rectangle-vertical',
             quality: 80,
             maxHeight: 768,
-            maxWidth: 1024
-        },
-        {
-            suffix: '-rectangle-horizontal',
-            quality: 80,
-            maxHeight: 1024,
-            maxWidth: 768
-        },
-        {
-            suffix: '-square',
-            maxHeight: 320,
-            maxWidth: 320
+            maxWidth: 1024,
+            force: false // Force scale up if the configuration size greater than image actual size
         }
     ]
 };
 
 var uploader = new Uploader('kiettv-s3', opts);
-
 uploader.upload('/home/kiettv/Downloads/test.jpeg')
     .then(function(ret){
         console.info(ret);
